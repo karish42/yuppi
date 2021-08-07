2a5c4485a486d9ca14e5c3f9352601b80b4a04db
karish42 <erickarish42@gmail.com>
2021-08-07 17:30:29 +0300

    third commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn yuppi.wsgi
--- yuppi/settings.py
+++ yuppi/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'yuppi.urls'
@@ -126,6 +127,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,"media")
 
 STATIC_URL="/static/"
 STATIC_ROOT=os.path.join(BASE_DIR,"static")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
